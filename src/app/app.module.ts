134d85dec81aafa2c0aaaf1f27ba1ff19557ec69
juaneins <ballesterosjg@gmail.com>
2019-10-11 16:32:46 -0500

    implementando directivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product.component';
 import { CartComponent } from './cart/cart.component';
 import { ExponentialPipe } from './exponential.pipe';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent, CartComponent, ExponentialPipe],
+  declarations: [AppComponent, ProductComponent, CartComponent, ExponentialPipe, HighlightDirective],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
