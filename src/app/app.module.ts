f812f3a1040b2a54ea01156d0095c409aed20830
juaneins <ballesterosjg@gmail.com>
2019-10-11 17:41:42 -0500

    Organizando el proyecto en componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,13 @@ import { ProductComponent } from './components/product.component';
 import { CartComponent } from './cart/cart.component';
 import { ExponentialPipe } from './exponential.pipe';
 import { HighlightDirective } from './highlight.directive';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent, CartComponent, ExponentialPipe, HighlightDirective],
+  declarations: [AppComponent, ProductComponent, CartComponent, ExponentialPipe, HighlightDirective, HomeComponent, ProductsComponent, ContactComponent, DemoComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.scss']
+})
+export class DemoComponent implements OnInit {
+  title = 'platzi-store';
+  usuario = 'juan';
+
+  nombres = ['Juan', 'Pedro', 'José'];
+
+  numero: number;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  addItem() {
+    this.nombres.push('Agregar ítem');
+  }
+
+  deleteItem(index: number) {
+    this.nombres.splice(index, 1);
+  }
+}
