c0e43ce9e7185b869c242230ddc6ea9e8cd03c40
juaneins <ballesterosjg@gmail.com>
2019-10-11 16:02:24 -0500

    Estilos de producto y uso de pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent],
+  declarations: [AppComponent, ProductComponent, CartComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -3,11 +3,14 @@ import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product',
-  templateUrl: './product.component.html'
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit, DoCheck, OnDestroy {
   @Input() product: Product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+  today = new Date();
 
   constructor() {
     console.log('1. ProductComponent constructor');
@@ -19,15 +22,15 @@ export class ProductComponent implements OnInit, DoCheck, OnDestroy {
   } */
 
   ngOnInit(): void {
-      console.log('3. ngOnInit');
+    console.log('3. ngOnInit');
   }
 
   ngDoCheck(): void {
-      console.log('4. ngDoCheck');
+    console.log('4. ngDoCheck');
   }
 
   ngOnDestroy(): void {
-      console.log('5. ngOnDestroy');
+    console.log('5. ngOnDestroy');
   }
 
   addCart() {
