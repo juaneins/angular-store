46de6b4742fcbb6dae21ad45e0df5ba8a4bbb1d3
juaneins <ballesterosjg@gmail.com>
2019-10-11 08:35:43 -0500

    Creando el componente generico producto, ciclo de vida de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  declarations: [AppComponent, ProductComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/product.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges, OnInit, DoCheck, OnDestroy } from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html'
+})
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
+  @Input() product: Product;
+  @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+  constructor() {
+    console.log('1. ProductComponent constructor');
+  }
+
+  /* ngOnChanges(change: SimpleChanges): void {
+    console.log('2. ngOnChanges');
+    console.log(change);
+  } */
+
+  ngOnInit(): void {
+      console.log('3. ngOnInit');
+  }
+
+  ngDoCheck(): void {
+      console.log('4. ngDoCheck');
+  }
+
+  ngOnDestroy(): void {
+      console.log('5. ngOnDestroy');
+  }
+
+  addCart() {
+    console.log('Agregar al carrito');
+    this.productClicked.emit(this.product.id);
+  }
+}
