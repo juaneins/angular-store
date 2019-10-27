53c25c043d3ba266fd9e0e9cf02ccad67fe911f4
juaneins <ballesterosjg@gmail.com>
2019-10-27 13:20:57 -0500

    Guardianes v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { DemoComponent } from './demo/demo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import {LayoutComponent} from './layout/layout.component';
+import { AdminGuard } from './guardians/admin.guard';
 
 
 const routes: Routes = [
@@ -32,6 +33,7 @@ const routes: Routes = [
       },
       {
         path: 'contact',
+        canActivate: [AdminGuard],
         component: ContactComponent
       }
     ]
