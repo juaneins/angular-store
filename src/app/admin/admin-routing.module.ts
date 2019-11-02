96430c468fd1168f533ac049eabae1d718b86bd0
juaneins <ballesterosjg@gmail.com>
2019-11-02 15:02:34 -0500

    angular schematics

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,17 +2,27 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductFormComponent } from './component/product-form/product-form.component';
 import { NavComponent } from './component/nav/nav.component';
-
+import { TableComponent } from './component/table/table.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: '',
     component: NavComponent,
-    children: [{
-      path: 'create',
-      component: ProductFormComponent
-    } 
-  ]
+    children: [
+      {
+        path: 'create',
+        component: ProductFormComponent
+      },
+      {
+        path: 'table',
+        component: TableComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ]
   }
 ];
 
@@ -20,4 +30,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
