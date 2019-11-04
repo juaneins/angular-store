60aa109af69fde90e2fd86eea082a4a9b955a271
juaneins <ballesterosjg@gmail.com>
2019-11-04 13:57:57 -0500

    Contador de productos con async

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/core/services/cart/cart.service';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,14 +10,23 @@ import { CartService } from 'src/app/core/services/cart/cart.service';
 })
 export class HeaderComponent implements OnInit {
 
-  total = 0;
+  //total = 0;
+  total$: Observable<number>;
 
   constructor(private cartService: CartService) { 
-    this.cartService.cart$.subscribe(products => {
+    /* this.cartService.cart$.subscribe(products => {
       console.log(products);
       this.total = products.length;
       console.log(products.length);
-    })
+    }); */
+   /*  this.cartService.cart$
+    .pipe(map(products => products.length))
+    .subscribe(total => {     
+      this.total = total;
+      console.log(total);
+    });  */
+    this.total$ = this.cartService.cart$
+      .pipe(map(products => products.length)); 
   }
 
   ngOnInit() {
