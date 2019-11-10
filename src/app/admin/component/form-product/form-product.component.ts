0e9bfb7b1ea8e8fc59d47473f8ae8135525b12d0
juaneins <ballesterosjg@gmail.com>
2019-11-10 07:24:13 -0500

    Subiendo imagen con fire storage

--- src/app/admin/component/form-product/form-product.component.ts
+++ src/app/admin/component/form-product/form-product.component.ts
@@ -3,6 +3,9 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ProductsService } from 'src/app/core/services/products/products.service';
 import { Router } from '@angular/router';
 import { MyValidators } from 'src/app/utils/validators';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form-product',
@@ -12,9 +15,10 @@ import { MyValidators } from 'src/app/utils/validators';
 export class FormProductComponent implements OnInit {
 
   formulario: FormGroup;
+  image$: Observable<any>;
 
   constructor(private formBuilder: FormBuilder, private productsService: ProductsService,
-    private router: Router) { 
+    private router: Router, private storage: AngularFireStorage) { 
     this.buildForm();
   }
 
@@ -46,6 +50,25 @@ export class FormProductComponent implements OnInit {
       );
     }
    
+  }
+
+  uploadFile(event) { 
+    const file = event.target.files[0];
+    console.log(file);
+    const name = file.name;
+    const fileRef = this.storage.ref(name);
+    const task = this.storage.upload(name, file);
+
+    task.snapshotChanges().pipe(
+      finalize(() => {
+        this.image$ = fileRef.getDownloadURL();
+        this.image$.subscribe(url => {
+          console.log(url);
+          this.formulario.get('image').setValue(url);
+        })
+      })
+    )
+    .subscribe();
   }
 
 }
